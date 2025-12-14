76face972010656c21a98ee8daee5fb400221035
Krishpavalankar <kp966@live.mdx.ac.uk>
2025-12-14 17:00:26 +0400

    Delete PythonProject directory

--- PythonProject/Week 9/app_db.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-BASE_PATH = Path("../Week 8/DATA")
-
-def get_table_from_csv(name):
-    path = BASE_PATH / f"{name}.csv"
-    return pd.read_csv(path)
